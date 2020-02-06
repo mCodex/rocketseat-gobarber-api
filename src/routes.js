8b54fab3956e9b35a4bb48f17442829da5ec51ff
Mateus Andrade <mat.andrade@live.com>
2020-02-06 20:20:42 -0300

    added user model/migration

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hello world' }));
+routes.get('/', async (req, res) => {
+  const newUser = await User.create({
+    name: 'Hi',
+    email: 'mateus@teste.com',
+    password_hash: '1234',
+  });
+
+  res.json(newUser);
+});
 
 export default routes;
